05d93017905cc076ad3ff62f5f6333033d4b3081
dsail <dsail@yandex.ru>
2023-12-17 19:29:00 +0300

    commit_3

---
+++ TS_app/CartManager.swift
@@ -0,0 +1,18 @@
+//
+//  CartManager.swift
+//  TS_app
+//
+//  Created by Eugene Zvyagin on 17.12.2023.
+//
+
+import SwiftUI
+
+struct CartManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartManager()
+}
---
+++ TS_app/View/CartView.swift
@@ -0,0 +1,18 @@
+//
+//  CartView.swift
+//  TS_app
+//
+//  Created by Eugene Zvyagin on 17.12.2023.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartView()
+}
