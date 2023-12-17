832b0c6678ea914d1a79c89ff052899309e44a73
dsail <dsail@yandex.ru>
2023-12-17 08:27:08 +0300

    Initial Commit

---
+++ TS_app/TS_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  TS_appApp.swift
+//  TS_app
+//
+//  Created by Eugene Zvyagin on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TS_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
